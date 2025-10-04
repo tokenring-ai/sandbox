e486e4ed70b58137501173370575048ede596ca0
Mark Dierolf <mdierolf@cloudquote.io>
2025-10-04 12:50:36 -0700

    Implemented auto-configuration, that allows the package to automatically install into TokenRing apps

--- index.ts
+++ index.ts
@@ -1,17 +1,46 @@
-import {TokenRingPackage} from "@tokenring-ai/agent";
-
+import {AgentTeam, TokenRingPackage} from "@tokenring-ai/agent";
+import DockerSandboxProvider from "@tokenring-ai/docker/DockerSandboxProvider";
+import {z} from "zod";
 import * as chatCommands from "./chatCommands.ts";
 import packageJSON from './package.json' with {type: 'json'};
+import SandboxService from "./SandboxService.ts";
 import * as tools from "./tools.ts";
+
+export const SandboxConfigSchema = z.object({
+  providers: z.record(z.string(), z.any()),
+  default: z.object({
+    provider: z.string()
+  }).optional()
+}).optional();
 
 export const packageInfo: TokenRingPackage = {
   name: packageJSON.name,
   version: packageJSON.version,
   description: packageJSON.description,
-  chatCommands,
-  tools
-};
+  install(agentTeam: AgentTeam) {
+    const config = agentTeam.getConfigSlice('sandbox', SandboxConfigSchema);
+    if (config) {
+      agentTeam.addTools(packageInfo, tools);
+      agentTeam.addChatCommands(chatCommands);
+      const sandboxService = new SandboxService();
+      agentTeam.addServices(sandboxService);
 
+      if (config.providers) {
+        for (const name in config.providers) {
+          const sandboxConfig = config.providers[name];
+          switch (sandboxConfig.type) {
+            case "docker":
+              sandboxService.registerSandboxProvider(name, new DockerSandboxProvider(sandboxConfig));
+              break;
+          }
+        }
+      }
+      if (config.default?.provider) {
+        sandboxService.setActiveSandboxProviderName(config.default.provider);
+      }
+    }
+  }
+};
 
 export {default as SandboxService} from "./SandboxService.ts";
 export {default as SandboxResource} from "./SandboxProvider.ts";
